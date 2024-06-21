d0898f611de455abd875dd6de2570caea4692972
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2024-06-21 15:17:02 -0300

    First Commit
    My First Java Challenge

---
+++ DesafiosJava/src/Desafio01.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Desafio01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a idade em anos: ");
+        int idadeAnos = sc.nextInt();
+
+        int idadeEmDias = idadeAnos * 365;
+        int idadeEmMeses = idadeAnos * 12;
+        
+        System.out.println("A idade em dias é: " + idadeEmDias + " dias");
+        System.out.println("A idade em meses é: " + idadeEmMeses + " meses");
+
+    }
+}
