a828e8a7c1aa0e9dcccfd9182335facc8bca54cb
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2024-06-26 14:37:56 -0300

    Commit
    Commit

--- DesafiosJava/src/Desafio04/SmartTv.java
+++ DesafiosJava/src/Desafio04/SmartTv.java
@@ -1,65 +1,65 @@
 package Desafio04;
 
 
-            public class SmartTv {
-                boolean ligada = false;
-                boolean desligada = true;
-                int volume = 10;
-                int canal;
+public class SmartTv {
+    boolean ligada = false;
+    boolean desligada = true;
+    int volume = 10;
+    int canal;
 
 
-                public SmartTv() {
-                }
+    public SmartTv() {
+    }
 
-                public SmartTv(boolean ligada, boolean desligada, int volume, int canal) {
-                    this.ligada = ligada;
-                    this.desligada = desligada;
-                    this.volume = volume;
-                    this.canal = canal;
-                }
+    public SmartTv(boolean ligada, boolean desligada, int volume, int canal) {
+        this.ligada = ligada;
+        this.desligada = desligada;
+        this.volume = volume;
+        this.canal = canal;
+    }
 
-                public boolean getLigada() {
-                    return ligada;
-                }
+    public boolean getLigada() {
+        return ligada;
+    }
 
-                public void setLigada(boolean ligada) {
-                    this.ligada = ligada;
-                }
+    public void setLigada(boolean ligada) {
+        this.ligada = ligada;
+    }
 
-                public boolean getDesligada() {
-                    return desligada;
-                }
+    public boolean getDesligada() {
+        return desligada;
+    }
 
-                public void setDesligada(boolean desligada) {
-                    this.desligada = desligada;
-                }
+    public void setDesligada(boolean desligada) {
+        this.desligada = desligada;
+    }
 
-                public int getVolume() {
-                    return volume;
-                }
+    public int getVolume() {
+        return volume;
+    }
 
-                public void setVolume(int volume) {
-                    this.volume = volume;
-                }
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
-                public int getCanal() {
-                    return canal;
-                }
+    public int getCanal() {
+        return canal;
+    }
 
-                public void setCanal(int canal) {
-                    this.canal = canal;
-                }
+    public void setCanal(int canal) {
+        this.canal = canal;
+    }
 
-                public void aumentarVolume(int novoVolume) {
-                    this.volume++;
+    public void aumentarVolume(int novoVolume) {
+        this.volume++;
 
-                }
+    }
 
-                public void diminuirVolume(int novoVolume) {
-                    this.volume--;
+    public void diminuirVolume(int novoVolume) {
+        this.volume--;
 
-                }
-            }
+    }
+}
 
 
 
--- DesafiosJava/src/Main.java
+++ DesafiosJava/src/Main.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class ain {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
