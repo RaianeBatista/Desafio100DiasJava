14d189a45edba32c9a0381e3e4c99a4aaeb5d417
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2024-07-14 16:58:42 -0300

    Commit indentação

--- DesafiosJava/src/edu/raiane/desafios100/java/desafio08/metodos/Calculadora.java
+++ DesafiosJava/src/edu/raiane/desafios100/java/desafio08/metodos/Calculadora.java
@@ -5,24 +5,20 @@ package edu.raiane.desafios100.java.desafio08.metodos;
         public void somarDoisNumeros() {
             System.out.println(10 + 10);
         }
-
         public void subtraiDoisNumeros() {
             System.out.println(21 - 2);
         }
-
         //Nesse caso, para executar esse método, precisa-se passar 2 valores inteiros
         //(int num1, int num2) são chamados de parâmetros
         public void multiplicaDoisNumeros(int num1, float num2) {
             System.out.println(num1 * num2);
         }
-
         public double divideDoisNumeros(double num1, double num2) {
             if (num2 == 0) {
                 return 0;
             }
             return num1 / num2;
         }
-
         public double divideDoisNumeros02(double num1, double num2) {
             // if (num2 == 0)
             if (num2 != 0) {
@@ -31,8 +27,6 @@ package edu.raiane.desafios100.java.desafio08.metodos;
             // System.out.println("Não existe divisão por zero");
             return 0;
         }
-
-
         public void imprimeDivisaoDeDoisNumeros(double num1, double num2) {
             if (num2 == 0) {
                 System.out.println("Não existe divisão por zero");
@@ -41,7 +35,6 @@ package edu.raiane.desafios100.java.desafio08.metodos;
             }
             System.out.println(num1 / num2);
         }
-
         public void alteraDoisNumeros(int numero1, int numero2) {
 
             numero1 = 99;
@@ -49,9 +42,7 @@ package edu.raiane.desafios100.java.desafio08.metodos;
             System.out.println("Dentro do alteraDoisNumeros");
             System.out.println("Num1=" + numero1);
             System.out.println("Num2=" + numero2);
-
         }
-
         public void somaArray(int[] numeros) {
             int soma = 0;
             for (int num : numeros) {
@@ -59,7 +50,6 @@ package edu.raiane.desafios100.java.desafio08.metodos;
             }
             System.out.println(soma);
         }
-
         public void somaVarArgs(int... numeros) {
             int soma = 0;
             for (int num : numeros) {
--- DesafiosJava/src/edu/raiane/desafios100/java/desafio08/metodos/CalculadoraTest.java
+++ DesafiosJava/src/edu/raiane/desafios100/java/desafio08/metodos/CalculadoraTest.java
@@ -19,9 +19,3 @@ package edu.raiane.desafios100.java.desafio08.metodos;
 
 
 
- /*  Calculadora calculadora = new Calculadora();
-    //array de 5 posições com indice de 0 a 4
-    int[] numeros = {1, 2, 3, 4, 5};
-            calculadora.somaArray(new int[]{1, 2, 3, 4, 5});
-            // calculadora.somaArray(numeros);
-            calculadora.somaVarArgs(1, 2, 3, 4, 5, 6, 7);*/
